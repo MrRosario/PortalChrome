13876ba984eb6698b110437ce1a52bdfe25409eb
Eugenio Rosario Mbi <eugenio.dorosario94@gmail.com>
2023-02-20 10:12:59 -0300

    new features

--- components/CardFeatured/index.js
+++ components/CardFeatured/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import Link from "next/link";
-import styles from './CardFeatured.module.css';
-import FallbackImage from '../../public/fallbackImg.svg'
+import styles from "./CardFeatured.module.css";
+import FallbackImage from "../../public/fallbackImg.svg";
 
 const CardFeatured = ({ isCardBig, img, title, goTopost }) => (
-    <Link href={encodeURIComponent(goTopost)} className={`card ${isCardBig ? styles.bigCard : styles.smallCard}`}>
-        <div className={styles.card__imageLayer}>
-            <img 
-                loading="lazy"
-                src={!img ? FallbackImage : img} 
-                alt={goTopost}
-            />
-        </div>
-        <div className={styles.card__title}>
-            <h2>{title}</h2>
-        </div>
-    </Link>
+  <Link
+    href={encodeURIComponent(goTopost)}
+    className={`card ${isCardBig ? styles.bigCard : styles.smallCard}`}
+  >
+    <div className={styles.card__imageLayer}>
+      <img
+        loading="lazy"
+        src={!img ? FallbackImage : img}
+        alt={!img ? "fallbackImage" : goTopost}
+      />
+    </div>
+    <div className={styles.card__title}>
+      <h2>{title}</h2>
+    </div>
+  </Link>
 );
 
 export default CardFeatured;
--- components/CardPost/index.js
+++ components/CardPost/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import styles from './CardPost.module.css';
+import styles from "./CardPost.module.css";
 
 const CardPost = ({ isCardBig, title, img, goTopost, alternativeText }) => (
-    <Link href={encodeURIComponent(goTopost)} className={`card ${styles.postCard} ${isCardBig ? styles.bigPostCard : styles.smallPostCard}`}>
-        <div className={styles.postCard__imageLayer}>
-            <img 
-                loading="lazy"
-                src={img} 
-                alt={alternativeText} 
-            />
-        </div>
-        <div className={styles.postCard__title}>
-            <h2>{title}</h2>
-        </div>
-    </Link>
+  <Link
+    href={encodeURIComponent(goTopost)}
+    className={`card ${styles.postCard} ${
+      isCardBig ? styles.bigPostCard : styles.smallPostCard
+    }`}
+  >
+    <div className={styles.postCard__imageLayer}>
+      <img
+        loading="lazy"
+        src={img}
+        alt={!img ? "imagePost" : alternativeText}
+      />
+    </div>
+    <div className={styles.postCard__title}>
+      <h2>{title}</h2>
+    </div>
+  </Link>
 );
 
 export default CardPost;
